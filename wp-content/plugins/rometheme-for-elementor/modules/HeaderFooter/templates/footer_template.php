f2fc315e786f67c0e70b82eed7eb3a5326aabfeb
Md Jewel Miah <jewelmm9@gmail.com>
2025-02-16 15:32:07 +0600

    Create README File

--- wp-content/plugins/rometheme-for-elementor/modules/HeaderFooter/templates/footer_template.php
+++
@@ -1,6 +0,0 @@
-<?php do_action('romethemekit_footer');?>
-</div><!-- #page -->
-<?php wp_footer(); ?>
-</body>
-
-</html>
--- wp-content/plugins/rometheme-for-elementor/view/welcome.php
+++
@@ -1,5 +0,0 @@
-<div class="spacer-2"></div>
-
-<?php
-require_once(RomeTheme::plugin_dir() . 'view/header.php');
-require_once(RomeTheme::plugin_dir() . 'view/dashboard.php');
